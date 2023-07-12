04a66d3a6e7918274936aec3fa4bf7b1cb1e7b6b
JatinAggarwal04 <135434558+JatinAggarwal04@users.noreply.github.com>
2023-07-13 00:55:43 +0530

    Update script_login.js

--- script_login.js
+++ script_login.js
@@ -15,3 +15,28 @@ loginForm.addEventListener('submit', function(event) {
   
   window.location.href = 'discord_clone.html';
 });
+
+
+var loginForm = document.getElementById('loginForm');
+var loadingOverlay = document.getElementById('loadingOverlay');
+
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  
+  loadingOverlay.classList.add('loading');
+
+  
+  setTimeout(function() {
+    var loginEmail = loginEmailInput.value;
+    var loginPassword = loginPasswordInput.value;
+
+    alert('Login Successful!');
+    loginForm.reset();
+
+  
+    loadingOverlay.classList.remove('loading');
+
+    window.location.href = 'discord_clone.html';
+  }, 2000); 
+});
