cdee0d7b17725e6ed970383343ef080ee0251676
JatinAggarwal04 <135434558+JatinAggarwal04@users.noreply.github.com>
2023-07-13 00:56:40 +0530

    Update script_registration.js

--- script_registration.js
+++ script_registration.js
@@ -2,6 +2,7 @@ var registrationForm = document.getElementById('registrationForm');
 var emailInput = document.getElementById('emailInput');
 var passwordInput = document.getElementById('passwordInput');
 var confirmPasswordInput = document.getElementById('confirmPasswordInput');
+var usernameInput = document.getElementById('usernameInput');
 
 registrationForm.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -9,6 +10,7 @@ registrationForm.addEventListener('submit', function(event) {
   var email = emailInput.value;
   var password = passwordInput.value;
   var confirmPassword = confirmPasswordInput.value;
+  var username = usernameInput.value;
 
   
   alert('Registration Successful!');
