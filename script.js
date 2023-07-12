70c42157945fc6e9d04f67e1e71e36b094083763
JatinAggarwal04 <135434558+JatinAggarwal04@users.noreply.github.com>
2023-07-12 16:39:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var messageInput = document.getElementById('messageInput');
+var sendButton = document.getElementById('sendButton');
+var chatContainer = document.getElementById('chatContainer');
+
+sendButton.addEventListener('click', function() {
+  var message = messageInput.value;
+  if (message.trim() !== '') {
+    addMessage('You', getCurrentTime(), message);
+    messageInput.value = '';
+  }
+});
+
+function addMessage(username, timestamp, message) {
+  var messageElement = document.createElement('div');
+  messageElement.classList.add('message');
+
+  var usernameElement = document.createElement('span');
+  usernameElement.classList.add('username');
+  usernameElement.textContent = username;
+
+  var timestampElement = document.createElement('span');
+  timestampElement.classList.add('timestamp');
+  timestampElement.textContent = timestamp;
+
+  var messageContentElement = document.createElement('p');
+  messageContentElement.textContent = message;
+
+  messageElement.appendChild(usernameElement);
+  messageElement.appendChild(timestampElement);
+  messageElement.appendChild(messageContentElement);
+
+  chatContainer.appendChild(messageElement);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+function getCurrentTime() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  return hours + ':' + (minutes < 10 ? '0' : '') + minutes;
+}
+
+addMessage('John Doe', '12:34 PM', 'Hello, everyone!');
+addMessage('Jane Smith', '12:35 PM', 'Hey John! How are you doing?');
